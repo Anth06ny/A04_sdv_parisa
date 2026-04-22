975dfbfb576a2c3d153dcf244044e7cd005d9239
anth0 <anth06ny@gmail.com>
2026-04-22 12:10:05 +0200

    Fin TP Excezption

--- app/src/main/java/com/amonteiro/a04_sdv_parisa/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/amonteiro/a04_sdv_parisa/presentation/viewmodel/MainViewModel.kt
@@ -12,16 +12,18 @@ import kotlinx.coroutines.launch
 suspend fun main() {
     println("start")
     val viewModel = MainViewModel()
-    viewModel.loadWeathers("Nice")
+    viewModel.loadWeathers("")
     //Affichage de la liste (qui doit être remplie) contenue dans la donnée observable
     println("List : ${viewModel.dataList.value}")
 
-    while(viewModel.runInProgress.value) {
+    while (viewModel.runInProgress.value) {
         println("Attente...")
         delay(500)
     }
 
     println("List : ${viewModel.dataList.value}")
+    println("ErrorMessage : ${viewModel.errorMessage.value}" )
+
 
     //Pour que le programme s'arrête, inutile sur Android
     KtorWeatherApi.close()
@@ -32,19 +34,26 @@ class MainViewModel : ViewModel() {
     //MutableStateFlow est une donnée observable
     val dataList = MutableStateFlow(emptyList<WeatherEntity>())
     val runInProgress = MutableStateFlow(false)
+    val errorMessage = MutableStateFlow("")
 
 
     fun loadWeathers(cityName: String) {
 
         runInProgress.value = true
 
+
         viewModelScope.launch(Dispatchers.IO) {
-            dataList.value = KtorWeatherApi.loadWeathers(cityName)
-
-            runInProgress.value = false
+            try {
+                dataList.value = KtorWeatherApi.loadWeathers(cityName)
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+                errorMessage.value = e.message ?: "Une erreur est survenue"
+            }
+            finally {
+                runInProgress.value = false
+            }
         }
-
-
 
     }
 }
