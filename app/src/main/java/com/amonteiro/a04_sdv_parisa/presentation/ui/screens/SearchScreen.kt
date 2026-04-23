e912e09743b3897a918ebbdd8b3d534a956e6f5a
anth0 <anth06ny@gmail.com>
2026-04-23 09:26:04 +0200

    Ajouty thème de couleur

--- app/src/main/java/com/amonteiro/a04_sdv_parisa/presentation/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a04_sdv_parisa/presentation/ui/screens/SearchScreen.kt
@@ -1,18 +1,31 @@
 package com.amonteiro.a04_sdv_parisa.presentation.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import com.amonteiro.a04_sdv_parisa.R
+import com.amonteiro.a04_sdv_parisa.data.remote.WeatherEntity
 import com.amonteiro.a04_sdv_parisa.presentation.ui.theme.A04_sdv_parisaTheme
 import com.amonteiro.a04_sdv_parisa.presentation.viewmodel.MainViewModel
 
@@ -27,8 +40,6 @@ fun SearchScreenPreview() {
     //Utilisé par exemple dans MainActivity.kt sous setContent {...}
     A04_sdv_parisaTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-
             SearchScreen(
                 modifier = Modifier.padding(innerPadding)
             )
@@ -38,21 +49,43 @@ fun SearchScreenPreview() {
 
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
-    Column(modifier = modifier) {
-        println("SearchScreen()")
-        Text(text = "Text1", fontSize = 20.sp)
-        Spacer(Modifier.size(8.dp))
-        Text(text = "Text2", fontSize = 14.sp)
+    Column(modifier = modifier.fillMaxSize()) {
 
         val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
 
         list.forEach {
-            PictureRowItem(it.name)
+            PictureRowItem(data = it)
         }
     }
 }
 
-@Composable
-fun PictureRowItem(text: String) {
-    Text(text = text, fontSize = 20.sp)
+@Composable //Composable affichant 1 élément
+fun PictureRowItem(modifier: Modifier = Modifier, data: WeatherEntity) {
+    Row(modifier = modifier.background(MaterialTheme.colorScheme.tertiaryContainer).fillMaxWidth()) {
+        //Permission Internet nécessaire
+        AsyncImage(
+            model = data.weather.firstOrNull()?.icon,
+            //Pour aller le chercher dans string.xml R de votre package com.nom.projet
+            //contentDescription = getString(R.string.picture_of_cat),
+            //En dur
+            contentDescription = "une photo de chat",
+            contentScale = ContentScale.Fit,
+
+            //Pour toto.png. Si besoin de choisir l'import pour la classe R, c'est celle de votre package
+            //Image d'échec de chargement qui sera utilisé par la preview
+            error = painterResource(R.drawable.error),
+            //Image d'attente.
+            //placeholder = painterResource(R.drawable.toto),
+
+            onError = { println(it) },
+            modifier = Modifier
+                .heightIn(max = 100.dp)
+                .widthIn(max = 100.dp)
+        )
+        Column(modifier= Modifier.padding(10.dp)) {
+            Text(text = data.name, fontSize = 20.sp)
+            Text(text = data.getResume().take(20) + "...", fontSize = 14.sp, color = MaterialTheme.colorScheme.onTertiaryContainer)
+        }
+
+    }
 }
